8dd98223cfda592007ff605c27410e2ebe16ca8f
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-08-16 16:35:22 +0300

    Catalog view created
    - Created static catalog view, where the notes are gonna be stored later on

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ import page from './lib/page.mjs';
 import { addRender } from './middlewares/render.js';
 import { addSession } from './middlewares/session.js';
 import { getUserData } from './utils.js';
+import { catalogView } from './views/catalog.js';
 import { createView } from './views/create.js';
 
 page(addRender(document.querySelector('main')));
 page(addSession(getUserData));
 
-page('/', '/create');
-page('/notes', () => console.log('Catalog'))
+page('/', '/notes');
+page('/notes', catalogView)
 page('/notes/:id', ({params: {id}}) => console.log('details', id))
 page('/create', createView)
 
---
+++ src/views/catalog.js
@@ -0,0 +1,32 @@
+import { html } from "../lib/lit-html.js";
+import * as noteService from '../data/note.js'
+import { repeat } from '../lib/directives/repeat.js'
+
+const catalogTemplate = (list) => html`
+    <h1>Routes</h1>
+    ${list}
+`; 
+
+const listTemplate = (notes) => html`
+    <section>
+        ${repeat(notes, n => n.objectId, noteCard )}
+    </section>
+`;
+
+const noteCard = (note) => html`
+    <article class="note-card">
+        <h3>${note.name}</h3>
+        <p>Location: ${note.location}</p>
+        <p>Rating: ${note.rating}</p>
+        <p>Description: ${note.description}</p>
+        <p><a class="action" href="/notes/${note.objectId}">View Details</a></p>
+    </article>
+`;
+
+export async function catalogView(ctx) {
+    ctx.render(catalogTemplate(html`<p>Loading... &hellip;</p>`));
+
+    const { results: notes } = await noteService.getAll();
+
+    ctx.render(catalogTemplate(listTemplate(notes)));
+};
