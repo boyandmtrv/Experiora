02195bf7b55e038c45ac7c113e3e57fd4c45349c
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-08-17 17:11:55 +0300

    Home template design started
    - Background picture added
    - main paragraph styled
    - navbar styled

--- src/views/home.js
+++ src/views/home.js
@@ -1,9 +1,11 @@
 import { html } from "../lib/lit-html.js";
  
 const homeTemplate = () => html`
-    <h1>Welcome to Experiora.</h1>
-    <p>Find and create the best routes in your country. <a href="/notes">Browse routes</a></p>
-    <p>Any past experience? <a href="/create">Place it right away!</a></p>
+    <div class="content">
+        <h1>Welcome to Experiora.</h1>
+        <p>Find and create the best routes in your country. <a href="/notes" class="route-link">Browse routes</a></p>
+        <p>Any past experience? <a href="/create" class="create-link">Place it right away!</a></p>
+    </div>
 `; 
 
 export function homeView(ctx) {
