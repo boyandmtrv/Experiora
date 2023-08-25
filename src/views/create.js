96a22a12a2ccca04cef27c79b334d5f59ea60086
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2023-08-25 12:22:17 +0300

    Create section style created and modified

--- src/views/create.js
+++ src/views/create.js
@@ -3,26 +3,29 @@ import * as noteService from '../data/note.js'
 import { submitHandler } from "../utils.js";
 
 const createTemplate = (onSubmit) => html`
-<div class="create-box">
-        <form @submit=${onSubmit}>
-        <div class="input-create-box">
-        <input type="text" name="name" placeholder="Route-name" required>
-        </div>
-        <div class="input-create-box">
-            <input type="text" name="location" placeholder="Location" required>    
-        </div>
-        <div class="input-create-box">
-            <input type="number" name="rating" placeholder="Rating" required>    
-        </div>
-        <div class="input-create-box">
-            <textarea name="description" placeholder="Description" required></textarea>
-        </div>
-            <button class="create-btn">Create route</button>
-            <div class="create-link">
-                <p>If you would like to browse routes, please click &#x2022 <a href="/notes">//Here</a></p>
+<section class="create-section">
+    <div class="create-wrapper">
+            <form @submit=${onSubmit}>
+            <h1>Share your Experience</h1>
+            <div class="input-box">
+            <input type="text" name="name" placeholder="Route-name" required>
             </div>
-        </form>
-</div>`;
+            <div class="input-box">
+                <input type="text" name="location" placeholder="Location" required>    
+            </div>
+            <div class="input-box">
+            <input type="number" inputmode="numeric" name="rating" placeholder="Rating" required>    
+            </div>
+            <div class="input-box">
+                <textarea name="description" placeholder="Description" required></textarea>
+            </div>
+                <button type="submit" class="btn">Create route</button>
+                <div class="register-link">
+                    <p>If you would like to browse routes, please click &#9679 <a href="/notes">//Here</a></p>
+                </div>
+            </form>
+    </div>
+</section>`;
 
 export function createView(ctx) {
     ctx.render(createTemplate(submitHandler(onSubmit)));
@@ -36,7 +39,7 @@ export function createView(ctx) {
             return alert('Please note, the rating is between 0 and 5!')
         };
         
-        if (name == '' || location == '' || description == '' || Number.isNaN(rating)) {
+        if (name == '' || location == '' || description == '' || Number.isNaN(rating) || difficulty == '') {
             return alert('All fields are required!');
         };
 
